f2e92bf8d93a3d09cebedbd43e81614517543969
Nitin Kumar <32798252+NitinKumar26@users.noreply.github.com>
2021-03-19 00:05:55 +0530

    changes

---
+++ app/src/main/java/com/gadgetsfolk/kidsgk/activity/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.gadgetsfolk.kidsgk.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.gadgetsfolk.kidsgk.R;
+
+public class SignupActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
--- app/src/main/java/com/gadgetsfolk/kidsgk/activity/SplashActivity.kt
+++ app/src/main/java/com/gadgetsfolk/kidsgk/activity/SplashActivity.kt
@@ -9,16 +9,12 @@ import android.view.View
 import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-//import com.gadgetsfolk.kidsgk.BuildConfig
+import com.gadgetsfolk.kidsgk.BuildConfig
 import com.gadgetsfolk.kidsgk.R
 import com.gadgetsfolk.kidsgk.helper.HelperMethods.isNetworkAvailable
 import com.google.firebase.firestore.FirebaseFirestore
 import java.util.*
-import android.content.pm.PackageManager
-
-import android.content.pm.PackageInfo
-
-import android.R.attr.versionCode
+import com.google.firebase.auth.FirebaseAuth
 
 class SplashActivity : AppCompatActivity() {
 
@@ -32,25 +28,10 @@ class SplashActivity : AppCompatActivity() {
 
         changeStatusBarColor() //Change Status Bar Color -> Transparent
         setContentView(R.layout.activity_splash)
-
-        try {
-            val pInfo: PackageInfo =
-                this.packageManager.getPackageInfo(this.packageName, 0)
-            val versionCode: Int = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                pInfo.longVersionCode.toInt() // avoid huge version numbers and you will be ok
-            } else {
-                pInfo.versionCode
-            }
-            //val versionCode = pInfo.longVersionCode
-            versionCodeApp = versionCode.toString()
-            if (isNetworkAvailable(this@SplashActivity)) checkVersionCode()
-            else Toast.makeText(this@SplashActivity, "Please check your Internet connection", Toast.LENGTH_SHORT).show()
-        } catch (e: PackageManager.NameNotFoundException) {
-            e.printStackTrace()
-        }
-
-
-
+        val versionCode = BuildConfig.VERSION_CODE
+        versionCodeApp = versionCode.toString()
+        if (isNetworkAvailable(this@SplashActivity)) checkVersionCode()
+        else Toast.makeText(this@SplashActivity, "Please check your Internet connection", Toast.LENGTH_SHORT).show()
     }
 
     /**
@@ -66,19 +47,32 @@ class SplashActivity : AppCompatActivity() {
 
     private fun checkVersionCode() {
         FirebaseFirestore.getInstance().collection("version_code")
-                .document("version_code")
-                .get()
-                .addOnSuccessListener { documentSnapshot ->
+            .document("version_code")
+            .get()
+            .addOnSuccessListener { documentSnapshot ->
                     versionCode = documentSnapshot["version_code"].toString()
                     if (versionCode.equals(versionCodeApp, ignoreCase = true)) {
-                        val SPLASH_TIME_OUT = 3000
-                        Handler().postDelayed({ //This method will be executed once the timer is over
-                            //Start your app main activity
-                            val intent = Intent(this@SplashActivity, BottomActivity::class.java)
-                            startActivity(intent)
-                            //Close the activity
-                            finish()
-                        }, SPLASH_TIME_OUT.toLong())
+                        val currentUser = FirebaseAuth.getInstance().currentUser
+                        if (currentUser != null){
+                            val SPLASH_TIME_OUT = 3000
+                            Handler().postDelayed({
+                                //This method will be executed once the timer is over
+                                //Start your app main activity
+                                val intent = Intent(this@SplashActivity, BottomActivity::class.java)
+                                startActivity(intent)
+                                //Close the activity
+                                finish()
+                            }, SPLASH_TIME_OUT.toLong())
+                        }else{
+                            //User is not login -->Redirect to Login/Signup Activity
+                            val SPLASH_TIME_OUT = 3000
+                            Handler().postDelayed({
+                                val intent = Intent(this@SplashActivity, SignupActivity::class.java)
+                                startActivity(intent)
+                                //close the activity
+                                finish()
+                            }, SPLASH_TIME_OUT.toLong())
+                        }
                     } else {
                         val intent = Intent(this@SplashActivity, UpdateVersionActivity::class.java)
                         startActivity(intent)
@@ -91,4 +85,5 @@ class SplashActivity : AppCompatActivity() {
         private var versionCodeApp: String? = null
         private var versionCode: String? = null
     }
+
 }
